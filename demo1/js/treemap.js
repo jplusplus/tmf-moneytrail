373af33ca8782bdabd17f3272e2a14d2c9975338
Ricardo Lafuente <r@manufacturaindependente.org>
2015-05-12 17:20:54 +0200

    First stab at resize-aware chart, ugly but works(tm)

--- demo1/js/treemap.js
+++ demo1/js/treemap.js
@@ -1,73 +1,83 @@
+var container = document.getElementById("main") 
 
-var margin = {top: 40, right: 10, bottom: 10, left: 10},
-//    width = 600 - margin.left - margin.right,
-//    height = 1000 - margin.top - margin.bottom;
-      width = document.getElementById("main").clientWidth,
-      height = document.getElementById("main").clientHeight;
+function drawTreemap() {
 
-var color = d3.scale.category20c();
+    // remove previous if it exists
+    d3.select('#treemap-container').remove();
 
-var scale = d3.scale.linear()
-                    .domain([2000000, 10000000000])
-                    .range([10, 200]);
+    var margin = {top: 40, right: 10, bottom: 10, left: 10},
+    //    width = 600 - margin.left - margin.right,
+    //    height = 1000 - margin.top - margin.bottom;
+          width = container.offsetWidth,
+          height = container.offsetHeight;
 
-var treemap = d3.layout.treemap()
-    .size([width, height])
-    //.sticky(true)
-    .sort(function comparator(a, b) { return b.amount - a.amount; })
-    .value(function(d) { return scale(d.amount); });
+    console.log([width, height]);
 
-var div = d3.select("#main").append("div")
-    .style("position", "relative")
-    .style("width", (width + margin.left + margin.right) + "px")
-    .style("height", (height + margin.top + margin.bottom) + "px")
-    .style("left", margin.left + "px")
-    .style("top", margin.top + "px");
+    var color = d3.scale.category20c();
 
-d3.json('/data/mockdata-truncated-01.json', function (error, data) {
-  var node = div.datum(data).selectAll(".node")
-        .data(treemap.nodes)
-      .enter().append("div")
-        .attr("class", "node")
-        .call(position)
-        .style("background", function(d) { return d.children ? null : color(d.title); })
-        .append("span") /* titles inside a span */
-            .text(function(d) { return d.children ? null : d.title;});
+    var scale = d3.scale.linear()
+                        .domain([2000000, 10000000000])
+                        .range([10, 200]);
 
-    var subnodes = node.each( function(d) { 
-      if (d.subnodes) { 
-        n = d3.select(this).selectAll(".subnode")
-            .data(d.subnodes)
+    var treemap = d3.layout.treemap()
+        .size([width, height])
+        //.sticky(true)
+        .sort(function comparator(a, b) { return b.amount - a.amount; })
+        .value(function(d) { return scale(d.amount); });
+
+    var div = d3.select("#main").append("div")
+        .style("position", "relative")
+        .style("width", (width + margin.left + margin.right) + "px")
+        .style("height", (height + margin.top + margin.bottom) + "px")
+        //.style("left", margin.left + "px")
+        //.style("top", margin.top + "px")
+        .attr("id", "treemap-container");
+
+    d3.json('/data/mockdata-truncated-01.json', function (error, data) {
+      var node = div.datum(data).selectAll(".node")
+            .data(treemap.nodes)
           .enter().append("div")
-
-            .style("position", "relative")
-            .style("width", "30px")
-            .style("height", "30px")
-            .style("left", "5px")
-            .style("top", "5px")
-
-            .attr("class", "subnode")
+            .attr("class", "node")
             .call(position)
-            .style("background", function(s) { return color(s.title); })
-            .text(function(s) { return s.title; });
-                  ;
-        console.log(n);
-        // n.subnodes.each( function(s) { console.log(s) });
-      } 
-    });
+            .style("background", function(d) { return d.children ? null : color(d.title); })
+            .append("span") /* titles inside a span */
+                .text(function(d) { return d.children ? null : d.title;});
 
-    d3.selectAll("input").on("change", function change() {
-      var value = this.value === "count"
-          ? function() { return 1; }
-          : function(d) { return scale(d.amount); };
+        var subnodes = node.each( function(d) { 
+          if (d.subnodes) { 
+            n = d3.select(this).selectAll(".subnode")
+                .data(d.subnodes)
+              .enter().append("div")
 
-      node
-          .data(treemap.value(value).nodes)
-        .transition()
-          .duration(1500)
-          .call(position);
+                .style("position", "relative")
+                .style("width", "30px")
+                .style("height", "30px")
+                .style("left", "5px")
+                .style("top", "5px")
+
+                .attr("class", "subnode")
+                .call(position)
+                .style("background", function(s) { return color(s.title); })
+                .text(function(s) { return s.title; });
+                      ;
+            console.log(n);
+            // n.subnodes.each( function(s) { console.log(s) });
+          } 
+        });
+
+        d3.selectAll("input").on("change", function change() {
+          var value = this.value === "count"
+              ? function() { return 1; }
+              : function(d) { return scale(d.amount); };
+
+          node
+              .data(treemap.value(value).nodes)
+            .transition()
+              .duration(1500)
+              .call(position);
+        });
     });
-});
+}
 
 function position() {
   this.style("left", function(d) { return d.x + "px"; })
@@ -75,3 +85,15 @@ function position() {
       .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
       .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
 }
+
+drawTreemap();
+
+d3.select(window).on('resize', resize); 
+
+function resize() {
+  width = container.clientWidth,
+  height = container.clientHeight;
+  console.log([width, height]);
+  drawTreemap();
+}
+
