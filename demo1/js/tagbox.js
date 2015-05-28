7083de3b334d37055ffc7ea66830468a0987387a
Ricardo Lafuente <r@manufacturaindependente.org>
2015-05-28 12:29:36 +0200

    Fix indentation

--- demo1/js/tagbox.js
+++ demo1/js/tagbox.js
@@ -1,13 +1,13 @@
- jQuery(function() {
-
-   // Load tag list
-   $.getJSON("data/tags.json", function(data) {
-     jQuery("#filters").autocomplete({
-       source: data.tags,
-       minLength: 0,
-     })
-     .focus(function () {
-       $(this).autocomplete("search");
-     });
-   });
+jQuery(function() {
+  // Load tag list and setup autocomplete
+  $.getJSON("data/tags.json", function(data) {
+    jQuery("#filters").autocomplete({
+      source: data.tags,
+      minLength: 0,
+    })
+    .focus(function () {
+      // Show suggestion box on focus
+      $(this).autocomplete("search");
+    });
+  });
 });
