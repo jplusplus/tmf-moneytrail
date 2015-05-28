cbc546ba18d0d699eb2fe096dc225681dd991bd6
Ricardo Lafuente <r@manufacturaindependente.org>
2015-05-28 12:25:52 +0200

    Refactor tagbox code and fix initial popup

--- demo1/js/tagbox.js
+++ demo1/js/tagbox.js
@@ -1,33 +1,13 @@
  jQuery(function() {
 
    // Load tag list
-   //
-
-   var names = "";
-   $.getJSON( "data/tags.json", function( data ) {  
-     names = data.tags;
+   $.getJSON("data/tags.json", function(data) {
+     jQuery("#filters").autocomplete({
+       source: data.tags,
+       minLength: 0,
+     })
+     .focus(function () {
+       $(this).autocomplete("search");
+     });
    });
-
-   // Autocomplete setup, most comes straight from the jQuery UI accent example
-   var accentMap = {
-     "á": "a",
-     "ö": "o"
-   };
-   var normalize = function( term ) {
-     var ret = "";
-     for ( var i = 0; i < term.length; i++ ) {
-       ret += accentMap[ term.charAt(i) ] || term.charAt(i);
-     }
-     return ret;
-   };
-   jQuery( "#filters" ).autocomplete({
-     // init
-     source: function( request, response ) {
-       var matcher = new RegExp( jQuery.ui.autocomplete.escapeRegex( request.term ), "i" );
-       response( jQuery.grep( names, function( value ) {
-         value = value.label || value.value || value;
-         return matcher.test( value ) || matcher.test( normalize( value ) );
-       }) );
-     }
-   });
- });
+});
