58b2c59123a554b044d973fa862102039dd99100
pirhoo <pierre.romera@gmail.com>
2015-06-02 11:36:40 +0200

    Added deploy task

--- demo1/gulpfile.js
+++ demo1/gulpfile.js
@@ -1,12 +1,21 @@
 var gulp = require('gulp');
-var browserSync = require('browser-sync');
+
+var $ = require('gulp-load-plugins')({
+  pattern: ['gulp-*', 'browser-sync']
+});
 
 gulp.task('browser-sync', function() {
-    browserSync({
+    $.browserSync({
         server: {
             baseDir: "./"
         }
     });
+});
+
+gulp.task('deploy', function() {
+  return gulp.src("**/*").pipe($.ghPages({
+    remoteUrl: "git@github.com:Pirhoo/pirhoo.wip.git"
+  }));
 });
 
 gulp.task('default', ['browser-sync']);
